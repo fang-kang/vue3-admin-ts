0d384a6bb4cef9fb212f14da10fde11786d5d2ef
fang-kang <1793980864@qq.com>
2021-07-23 14:11:09 +0800

    add:添加验证文件

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -406,3 +406,27 @@ function compare(prop: string | number, order: boolean) {
     }
   };
 }
+
+/** 
+ * @description: 获取url上参数
+ * @author: fangkang
+ * @param {string} url
+ * @returns {Object}
+ */
+ export function param2Obj(url: string): object {
+  const search = decodeURIComponent(url.split('?')[1]).replace(/\+/g, ' ')
+  if (!search) {
+    return {}
+  }
+  const obj :any= {}
+  const searchArr = search.split('&')
+  searchArr.forEach(v => {
+    const index = v.indexOf('=')
+    if (index !== -1) {
+      const name = v.substring(0, index)
+      const val = v.substring(index + 1, v.length)
+      obj[name] = val
+    }
+  })
+  return obj
+}
